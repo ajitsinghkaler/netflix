feb9c0caf3b53416b6e6a132c2dad46b73a8243d
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-01-25 16:55:27 +0530

    netflix jumbotron and footer

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { FooterContainer } from "./containers/footer";
+import { JumbotronContainer } from "./containers/jumbotron";
+
+export default function App() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
+}
