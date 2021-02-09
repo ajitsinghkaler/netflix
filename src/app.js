994fcf2c6cab665780101840ddc93fd8425d7e89
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-02-09 13:38:39 +0530

    added user to app

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import * as ROUTES from "./constants/routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Home, Signin, Signup, Browse } from "./pages";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 export default function App() {
-  const user = null;
+  const user = useAuthListener();
+  console.log(user);
   return (
     <Router>
       <IsUserRedirect
@@ -34,7 +36,6 @@ export default function App() {
       >
         <Home />
       </IsUserRedirect>
-      
     </Router>
   );
 }
